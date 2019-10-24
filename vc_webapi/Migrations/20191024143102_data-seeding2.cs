26c3d4f0241fdabf2afb6be15e02b9cd1a7e5f5b
amavin <237294@via.dk>
2019-10-24 16:36:41 +0200

    Added dummy data to db migrations

---
+++ vc_webapi/Migrations/20191024143102_data-seeding2.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vc_webapi.Migrations
+{
+    public partial class dataseeding2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Enrollments",
+                keyColumn: "Id",
+                keyValue: 100L,
+                column: "EnrollmentDateUtc",
+                value: new DateTime(2019, 7, 26, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(9383));
+
+            migrationBuilder.UpdateData(
+                table: "Enrollments",
+                keyColumn: "Id",
+                keyValue: 101L,
+                column: "EnrollmentDateUtc",
+                value: new DateTime(2019, 7, 26, 14, 31, 2, 255, DateTimeKind.Utc).AddTicks(31));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 100L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(6150));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 101L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(6701));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 102L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(7799));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 103L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(7800));
+
+            migrationBuilder.InsertData(
+                table: "UserSession",
+                columns: new[] { "Id", "SessionId", "UserId" },
+                values: new object[,]
+                {
+                    { 100L, 100L, 100L },
+                    { 101L, 101L, 100L },
+                    { 102L, 102L, 100L },
+                    { 103L, 103L, 100L }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 100L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(5127));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 101L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 15, 16, 2, 254, DateTimeKind.Utc).AddTicks(6065));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 102L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 16, 1, 2, 254, DateTimeKind.Utc).AddTicks(6133));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 103L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 16, 56, 2, 254, DateTimeKind.Utc).AddTicks(6134));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 104L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(6136));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 105L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 15, 16, 2, 254, DateTimeKind.Utc).AddTicks(6147));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 106L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 16, 1, 2, 254, DateTimeKind.Utc).AddTicks(6148));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 107L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 16, 56, 2, 254, DateTimeKind.Utc).AddTicks(6149));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 108L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(7788));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 109L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 15, 16, 2, 254, DateTimeKind.Utc).AddTicks(7792));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 110L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 16, 1, 2, 254, DateTimeKind.Utc).AddTicks(7793));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 111L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 16, 56, 2, 254, DateTimeKind.Utc).AddTicks(7794));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 112L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 14, 31, 2, 254, DateTimeKind.Utc).AddTicks(7795));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 113L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 15, 16, 2, 254, DateTimeKind.Utc).AddTicks(7796));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 114L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 16, 1, 2, 254, DateTimeKind.Utc).AddTicks(7797));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 115L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 16, 56, 2, 254, DateTimeKind.Utc).AddTicks(7798));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserSession",
+                keyColumn: "Id",
+                keyValue: 100L);
+
+            migrationBuilder.DeleteData(
+                table: "UserSession",
+                keyColumn: "Id",
+                keyValue: 101L);
+
+            migrationBuilder.DeleteData(
+                table: "UserSession",
+                keyColumn: "Id",
+                keyValue: 102L);
+
+            migrationBuilder.DeleteData(
+                table: "UserSession",
+                keyColumn: "Id",
+                keyValue: 103L);
+
+            migrationBuilder.UpdateData(
+                table: "Enrollments",
+                keyColumn: "Id",
+                keyValue: 100L,
+                column: "EnrollmentDateUtc",
+                value: new DateTime(2019, 7, 26, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(5934));
+
+            migrationBuilder.UpdateData(
+                table: "Enrollments",
+                keyColumn: "Id",
+                keyValue: 101L,
+                column: "EnrollmentDateUtc",
+                value: new DateTime(2019, 7, 26, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(6595));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 100L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(2787));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 101L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(3354));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 102L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(4428));
+
+            migrationBuilder.UpdateData(
+                table: "Session",
+                keyColumn: "Id",
+                keyValue: 103L,
+                column: "Date",
+                value: new DateTime(2019, 10, 24, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(4430));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 100L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(1657));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 101L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 14, 57, 4, 951, DateTimeKind.Utc).AddTicks(2673));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 102L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 15, 42, 4, 951, DateTimeKind.Utc).AddTicks(2741));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 103L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 24, 16, 37, 4, 951, DateTimeKind.Utc).AddTicks(2743));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 104L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(2744));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 105L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 14, 57, 4, 951, DateTimeKind.Utc).AddTicks(2755));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 106L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 15, 42, 4, 951, DateTimeKind.Utc).AddTicks(2756));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 107L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 31, 16, 37, 4, 951, DateTimeKind.Utc).AddTicks(2757));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 108L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(4416));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 109L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 14, 57, 4, 951, DateTimeKind.Utc).AddTicks(4421));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 110L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 15, 42, 4, 951, DateTimeKind.Utc).AddTicks(4423));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 111L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 10, 25, 16, 37, 4, 951, DateTimeKind.Utc).AddTicks(4424));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 112L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 14, 12, 4, 951, DateTimeKind.Utc).AddTicks(4425));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 113L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 14, 57, 4, 951, DateTimeKind.Utc).AddTicks(4425));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 114L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 15, 42, 4, 951, DateTimeKind.Utc).AddTicks(4426));
+
+            migrationBuilder.UpdateData(
+                table: "Videos",
+                keyColumn: "Id",
+                keyValue: 115L,
+                column: "RecordTimeUtc",
+                value: new DateTime(2019, 11, 1, 16, 37, 4, 951, DateTimeKind.Utc).AddTicks(4427));
+        }
+    }
+}
---
+++ vc_webapi/Migrations/Authentication/20191024132335_data-seeding.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vc_webapi.Migrations.Authentication
+{
+    public partial class dataseeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e575", 0, "4dd9ccc4-5c68-4850-8eb3-c46833905564", "some@mail.com", true, false, null, "SOME@MAIL.COM", "TESTSTUDENT", "AQAAAAEAACcQAAAAEAW7GJ1T+Sxx2CNrllEprqHTT3PvQGRcUYYteEz8CUXVqApHlT//vxMK0HpzwHGLAw==", null, false, "", false, "teststudent" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserClaims",
+                columns: new[] { "Id", "ClaimType", "ClaimValue", "UserId" },
+                values: new object[] { 100, "UserName", "teststudent", "a18be9c0-aa65-4af8-bd17-00bd9344e575" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserClaims",
+                keyColumn: "Id",
+                keyValue: 100);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575");
+        }
+    }
+}
