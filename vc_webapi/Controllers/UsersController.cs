fae70cb81fb108930af982052431f0b9fe9722c9
Sara Nunes <saramcs16@gmail.com>
2019-11-20 18:39:20 +0100

    added users controller

---
+++ vc_webapi/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using vc_webapi.Data;
+
+namespace vc_webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly Vc_webapiContext db;
+
+        public UsersController(Vc_webapiContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetUser([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await db.Users.SingleOrDefaultAsync(c => c.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user.Id);
+        }
+    }
+}
